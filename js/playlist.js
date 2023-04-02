57dfb537e914f0249cd1f324b25a787be64b96e7
WolfStack11 <121760322+WolfStack11@users.noreply.github.com>
2023-04-02 17:20:48 +0300

    Project files added

---
+++ js/playlist.js
@@ -0,0 +1,73 @@
+let songs = [
+    {
+        name: "6 Shots",
+        artist: "NEFFEX",
+        img: "img1",
+        audio: "NEFFEX - 6 Shots",
+        background: "#4287f5"
+    },
+
+    {
+        name: "On my own",
+        artist: "Jaden",
+        img: "img2",
+        audio: "Jaden - On My Own ft. Kid Cudi",
+        background: "#42f58a"
+    },
+
+    {
+        name: "Law",
+        artist: "CZAER",
+        img: "img3",
+        audio: "LAW (Prod. by Czaer)"
+    },
+
+    {
+        name: "Fearless",
+        artist: "NEFFEX",
+        img: "img4",
+        audio: "NEFFEX - Fearless"
+    },
+
+    {
+        name: "New thing",
+        artist: "ZICO",
+        img: "img5",
+        audio: "New thing (Prod. by ZICO)"
+    },
+
+    {
+        name: "Bad place",
+        artist: "The Hunna",
+        img: "img6",
+        audio: "The Hunna - Bad Place"
+    },
+
+    {
+        name: "Fight back",
+        artist: "NEFFEX",
+        img: "img7",
+        audio: "NEFFEX - Fight Back"
+    },
+
+    {
+        name: "Feel invincible",
+        artist: "Skillet",
+        img: "img8",
+        audio: "Skillet - Feel Invincible"
+    },
+
+    {
+        name: "Grateful",
+        artist: "NEFFEX",
+        img: "img9",
+        audio: "NEFFEX - Grateful"
+    },
+
+    {
+        name: "Missing piece",
+        artist: "Vance Joy",
+        img: "img10",
+        audio: "Vance Joy - Missing Piece"
+    },
+]
---
+++ js/script.js
@@ -0,0 +1,180 @@
+const content = document.querySelector(".content"),
+Playimage = content.querySelector(".music-image img"),
+musicName = content.querySelector(".music-titles .name"),
+musicArtist = content.querySelector(".music-titles .artist"),
+Audio = document.querySelector(".main-song"),
+playBtn = content.querySelector(".play-pause"),
+playBtnIcon = content.querySelector(".play-pause span"),
+prevBtn = content.querySelector("#prev"),
+nextBtn = content.querySelector("#next"),
+progressBar = content.querySelector(".progress-bar"),
+progressDetails = content.querySelector(".progress-details"),
+repeatBtn = content.querySelector("#repeat"),
+shuffleBtn = content.querySelector("#shuffle");
+
+let wave = document.getElementById('wave');
+let volume_slider = document.querySelector('.volume_slider');
+
+
+let index = 1;
+
+window.addEventListener("load", () => {
+    loadData(index);
+});
+
+function loadData(indexValue){
+    musicName.innerHTML = songs[indexValue - 1].name;
+    musicArtist.innerHTML = songs[indexValue - 1].artist;
+    Playimage.src = "Img/"+songs[indexValue - 1].img+".jpg";
+    Audio.src = "Music/"+songs[indexValue - 1].audio+".mp3";
+    random_bg_color();
+}
+
+
+// BACKGROUND
+function random_bg_color(){
+    let hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e'];
+    let a;
+
+    function populate(a){
+        for(let i=0; i<6; i++){
+            let x = Math.round(Math.random() * 14);
+            let y = hex[x];
+            a += y;
+        }
+        return a;
+    }
+    let Color1 = populate('#');
+    let Color2 = populate('#');
+    var angle = 'to right';
+
+    let gradient = 'linear-gradient(' + angle + ',' + Color1 + ', ' + Color2 + ")";
+    document.body.style.background = gradient;
+}
+
+
+//PLAY PAUSE
+playBtn.addEventListener("click", ()=>{
+    const isMusicPaused = content.classList.contains("paused")
+    if(isMusicPaused){
+        pauseSong();
+    }
+    else{
+        playSong();
+    }
+})
+
+function playSong(){
+    content.classList.add("paused");
+    playBtnIcon.innerHTML = "pause";
+    wave.classList.add('loader');
+    Audio.play();
+}
+
+function pauseSong(){
+    content.classList.remove("paused");
+    playBtnIcon.innerHTML = "play_arrow";
+    wave.classList.remove('loader');
+    Audio.pause();
+}
+
+
+// NEXT - PREV BUTTON
+nextBtn.addEventListener("click", () => {
+    nextSong();
+});
+
+prevBtn.addEventListener("click", () => {
+    prevSong();
+});
+
+function nextSong(){
+    index++;
+    if(index > songs.length){
+        index = 1;
+    }
+    else{
+        index = index;
+    }
+    loadData(index);
+    playSong();
+}
+
+function prevSong(){
+    index--;
+    if(index <= 0){
+        index = songs.length;
+    }
+    else{
+        index = index;
+    }
+    loadData(index);
+    playSong();
+}
+
+
+// TIME SLIDER
+Audio.addEventListener("timeupdate", (e) => {
+    const initialTime = e.target.currentTime; // Get current music time
+    const finalTime = e.target.duration; // Get music duration
+    let BarWidth = (initialTime / finalTime) * 100;
+    progressBar.style.width = BarWidth+"%";
+
+    progressDetails.addEventListener("click", (e) => {
+        let progressValue = progressDetails.clientWidth; // Get width of progress bar
+        let clickOffsetX = e.offsetX; // Get offset x value
+        let MusicDuration = Audio.duration; // Get total music durations
+
+        Audio.currentTime = (clickOffsetX / progressValue) * MusicDuration; 
+    })
+
+    //Timer Logic
+    Audio.addEventListener("loadeddata", () => {
+        let finalTimeData = content.querySelector(".final")
+
+
+        //Update finalDuration
+        let AudioDuration = Audio.duration;
+        let finalMinutes = Math.floor(AudioDuration / 60);  //floor - rounds up to the nearest whole number
+        let finalSeconds = Math.floor(AudioDuration % 60);
+        if(finalSeconds < 10){
+            finalSeconds = "0"+finalSeconds;
+        }
+        finalTimeData.innerText = finalMinutes + ":" + finalSeconds;
+    });
+
+    //Update Current Duration
+    let currentTimeData = content.querySelector(".current")
+    let CurrentTime = Audio.currentTime;
+    let currentMinutes = Math.floor(CurrentTime / 60);
+    let currentSeconds = Math.floor(CurrentTime % 60);
+    if(currentSeconds < 10){
+        currentSeconds = "0"+currentSeconds;
+    }
+    currentTimeData.innerText = currentMinutes + ":" + currentSeconds;
+
+    //repeat button logic 
+    repeatBtn.addEventListener("click", () => {
+        Audio.currentTime = 0;
+    })
+});
+
+//Shuffle logic
+shuffle.addEventListener("click", () => {
+    var randIndex = Math.floor(Math.random() * songs.length) + 1; //Select random number between 1 and song array length
+    loadData(randIndex);
+    playSong();
+});
+
+Audio.addEventListener("ended", () => {
+    index++;
+    if(index > songs.length){
+        index = 1;
+    }
+    loadData(index);
+    playSong();
+});
+
+function setVolume(){
+    Audio.volume = volume_slider.value / 100;
+}
